e37acd440cb443c3bc9a0aa038cabef2de47148e
“Richard Middleton” <Richard@richardmiddleton.me>
2017-09-11 22:34:42 +0100

    tidy and comment js

--- js/main.js
+++ js/main.js
@@ -6,6 +6,105 @@ var firstVid;
 
 $(document).ready(function () {
 
+    // Load Videos On Load
+    loadVids(token);
+
+    //Load More Videos On Click
+    $('.load-more').on("click", function (e) {
+        e.preventDefault();
+        var next = $(this).attr('data-key');
+
+        if (next !== "") {
+            loadVids(next);
+        }
+    });
+
+
+    //Get Request
+    function loadVids(token) {
+        $.get(
+            'https://www.googleapis.com/youtube/v3/playlistItems', {
+                part: 'snippet',
+                key: key,
+                maxResults: 10,
+                playlistId: playlistId,
+                pageToken: token
+            },
+            function (data) {
+                var nextPage = data.nextPageToken;
+                resultsLeft(nextPage);
+                resultsLoop(data);
+            }
+        );
+    }
+
+    //Check if there are any results left
+    function resultsLeft(nextPage) {
+        if (nextPage === undefined) {
+            $('.load-more').attr("data-key", "").html('No More Result').addClass('disabled');
+        }
+        else {$('.load-more').attr("data-key", nextPage);}
+    }
+
+    //Loop through results and display them on the dom.
+    function resultsLoop(data){
+        $.each(data.items, function (i, item) {
+
+            var thumb = item.snippet.thumbnails.medium.url;
+            var title = item.snippet.title;
+            var desc = item.snippet.description.substring(0, 100);
+            var vid = item.snippet.resourceId.videoId;
+
+            $('#results').append(`
+            <article id="item${i + 1}" class="item">
+                <div class="thumb-container">
+                    <img src="${thumb}" class="thumb" data-key="${vid}">
+                </div>
+                <div class="vid-details">
+                    <h3>${title}</h3>
+                    <p class="description">${desc}</p>
+                </div>
+            </article>`);
+        });
+    }
+
+
+    $('#item1').children('.thumb-container').children('.thumb').attr('data-key');
+
+    console.log(firstVid);
+
+    $('#video').html(`<iframe class="video" src="https://www.youtube.com/embed/STwoa-9jxi0?rel=0&amp;controls=1&amp&amp;showinfo=0&amp;modestbranding=0" frameborder="0" allowfullscreen></iframe>`);
+
+
+    // Aspect ratio for video 
+    var $allVideos = $("iframe[src^='//player.vimeo.com'], iframe[src^='//www.youtube.com'], object, embed"),
+        $fluidEl = $("figure");
+
+    $allVideos.each(function () {
+
+        $(this)
+            // jQuery .data does not work on object/embed elements
+            .attr('data-aspectRatio', this.height / this.width)
+            .removeAttr('height')
+            .removeAttr('width');
+    });
+
+    $(window).resize(function () {
+
+        var newWidth = $fluidEl.width();
+        $allVideos.each(function () {
+            var $el = $(this);
+            $el
+                .width(newWidth)
+                .height(newWidth * $el.attr('data-aspectRatio'));
+        });
+    }).resize();
+});
+
+
+
+
+
     // GET TITLE FROM YOUTUBE PLAYLIST
     // $.get(
     //     'https://www.googleapis.com/youtube/v3/playlists', {
@@ -22,104 +121,3 @@ $(document).ready(function () {
     //         });
     //     }
     // )
-
-    loadVids(token);
-
-    $('.load-more').on("click", function (e) {
-        e.preventDefault();
-        var next = $(this).attr('data-key');
-
-        if (next !== "") {
-            token = next;
-            console.log(token);
-            loadVids(token);
-        }
-    });
-
-
-    function loadVids(token) {
-
-        $.get(
-            'https://www.googleapis.com/youtube/v3/playlistItems', {
-                part: 'snippet',
-                key: key,
-                maxResults: 10,
-                playlistId: playlistId,
-                pageToken: token
-            },
-            function (data) {
-                console.log(data);
-                var nextPage = data.nextPageToken;
-
-                resultsLeft(nextPage);
-                resultsLoop(data);
-            }
-        );
-    }
-
-    function resultsLeft(nextPage) {
-        if (nextPage === undefined) {
-            $('.load-more').attr("data-key", "").html('No More Result').addClass('disabled');
-        }
-        else {$('.load-more').attr("data-key", nextPage);}
-    }
-
-    function resultsLoop(data){
-        $.each(data.items, function (i, item) {
-
-            var thumb = item.snippet.thumbnails.medium.url;
-            var title = item.snippet.title;
-            var desc = item.snippet.description.substring(0, 100);
-            var vid = item.snippet.resourceId.videoId;
-
-            $('#results').append(`<article id="item${i + 1}" class="item">
-        <div class="thumb-container">
-            <img src="${thumb}" class="thumb" data-key="${vid}">
-        </div>
-        <div class="vid-details">
-            <h3>${title}</h3>
-            <p class="description">${desc}</p>
-        </div>
-    </article>`);
-
-
-
-        });
-    }
-
-
-    $('#item1').children('.thumb-container').children('.thumb').attr('data-key');
-
-    console.log(firstVid);
-
-    $('#video').html(`<iframe class="video" src="https://www.youtube.com/embed/STwoa-9jxi0?rel=0&amp;controls=1&amp&amp;showinfo=0&amp;modestbranding=0" frameborder="0" allowfullscreen></iframe>`);
-
-
-    var $allVideos = $("iframe[src^='//player.vimeo.com'], iframe[src^='//www.youtube.com'], object, embed"),
-        $fluidEl = $("figure");
-
-    $allVideos.each(function () {
-
-        $(this)
-            // jQuery .data does not work on object/embed elements
-            .attr('data-aspectRatio', this.height / this.width)
-            .removeAttr('height')
-            .removeAttr('width');
-
-    });
-
-    $(window).resize(function () {
-
-        var newWidth = $fluidEl.width();
-        $allVideos.each(function () {
-
-            var $el = $(this);
-            $el
-                .width(newWidth)
-                .height(newWidth * $el.attr('data-aspectRatio'));
-
-        });
-
-    }).resize();
-
-});
