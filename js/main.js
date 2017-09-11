213763f90e756fa17d345cca909ca2fd4bf63542
“Richard Middleton” <Richard@richardmiddleton.me>
2017-09-11 22:29:49 +0100

    split js

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ var channelId = 'UCX-b8pihLF-Q9mOhpTILfjw';
 var key = 'AIzaSyDcwEYXvmlat_s2gYCTUjOPJlZywkIwu10';
 var playlistId = "PL7Mh8U8DxyOG0zVj_5cpXYgFxGZ57CcJk";
 var token = null;
+var firstVid;
 
 $(document).ready(function () {
 
@@ -24,14 +25,11 @@ $(document).ready(function () {
 
     loadVids(token);
 
-
     $('.load-more').on("click", function (e) {
         e.preventDefault();
         var next = $(this).attr('data-key');
 
-        if (next === "") {
-        }
-        else {
+        if (next !== "") {
             token = next;
             console.log(token);
             loadVids(token);
@@ -52,38 +50,49 @@ $(document).ready(function () {
             function (data) {
                 console.log(data);
                 var nextPage = data.nextPageToken;
-                if (nextPage === undefined) {
 
-                    $('.load-more').attr("data-key", "");
-                    $('.load-more').html('No More Result').addClass('disabled');
-                }
-                else {
-                    $('.load-more').attr("data-key", nextPage);
-                }
-
-                $.each(data.items, function (i, item) {
-                    var thumb = item.snippet.thumbnails.medium.url;
-
-                    var title = item.snippet.title;
-                    var desc = item.snippet.description.substring(0, 100);
-                    var vid = item.snippet.resourceId.videoId;
-
-                    $('#results').append(`<article class="item">
-                <div class="thumb-container">
-                    <img src="${thumb}" class="thumb" data-key="${vid}">
-                </div>
-                <div class="vid-details">
-                    <h3>${title}</h3>
-                    <p class="description">${desc}</p>
-                </div>
-            </article>`);
-
-
-                });
+                resultsLeft(nextPage);
+                resultsLoop(data);
             }
         );
     }
 
+    function resultsLeft(nextPage) {
+        if (nextPage === undefined) {
+            $('.load-more').attr("data-key", "").html('No More Result').addClass('disabled');
+        }
+        else {$('.load-more').attr("data-key", nextPage);}
+    }
+
+    function resultsLoop(data){
+        $.each(data.items, function (i, item) {
+
+            var thumb = item.snippet.thumbnails.medium.url;
+            var title = item.snippet.title;
+            var desc = item.snippet.description.substring(0, 100);
+            var vid = item.snippet.resourceId.videoId;
+
+            $('#results').append(`<article id="item${i + 1}" class="item">
+        <div class="thumb-container">
+            <img src="${thumb}" class="thumb" data-key="${vid}">
+        </div>
+        <div class="vid-details">
+            <h3>${title}</h3>
+            <p class="description">${desc}</p>
+        </div>
+    </article>`);
+
+
+
+        });
+    }
+
+
+    $('#item1').children('.thumb-container').children('.thumb').attr('data-key');
+
+    console.log(firstVid);
+
+    $('#video').html(`<iframe class="video" src="https://www.youtube.com/embed/STwoa-9jxi0?rel=0&amp;controls=1&amp&amp;showinfo=0&amp;modestbranding=0" frameborder="0" allowfullscreen></iframe>`);
 
 
     var $allVideos = $("iframe[src^='//player.vimeo.com'], iframe[src^='//www.youtube.com'], object, embed"),
