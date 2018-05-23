de4f604ee030766ba923eb61dea8996a65bee50a
“Richard Middleton” <Richard@richardmiddleton.me>
2018-05-23 15:13:40 +0200

    updated for click through on next page

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ var playlistId = 'PL2fnLUTsNyq7A335zB_RpOzu7hEUcSJbB';
 var token = null;
 var firstVid = 'STwoa-9jxi0';
 
+
 $(document).ready(function () {
 
     // Event Listeners for articles
@@ -13,7 +14,6 @@ $(document).ready(function () {
             displayVid(id);
         }
     });
-
 
     // Load Videos On Page Load
     loadVids(token);
@@ -41,8 +41,6 @@ $(document).ready(function () {
                 pageToken: token
             },
             function (data) {
-                var id = data.items[0].snippet.resourceId.videoId;
-                displayVid(id);
                 var nextPage = data.nextPageToken;
                 resultsLeft(nextPage);
                 resultsLoop(data);
@@ -54,8 +52,9 @@ $(document).ready(function () {
     function resultsLeft(nextPage) {
         if (nextPage === undefined) {
             $('.load-more').attr("data-key", "").html('No More Results').addClass('disabled');
+        } else {
+            $('.load-more').attr("data-key", nextPage);
         }
-        else { $('.load-more').attr("data-key", nextPage); }
     }
 
     //Loop through results and display them on the dom.
@@ -66,10 +65,11 @@ $(document).ready(function () {
             var title = item.snippet.title;
             var vid = item.snippet.resourceId.videoId;
 
-            if(item.snippet.description.length > 100) {
+            if (item.snippet.description.length > 100) {
                 var desc = item.snippet.description.substring(0, 100) + ' ...';
+            } else {
+                var desc = item.snippet.description;
             }
-            else {var desc = item.snippet.description;}
 
             $('main').append(`
             <article id="item${i + 1}" class="item" data-key="${vid}">
@@ -93,14 +93,16 @@ $(document).ready(function () {
     //--------------------
     // Aspect ratio for video
     //-------------------- 
-    var $allVideos = $("iframe[src^='//player.vimeo.com'], iframe[src^='//www.youtube.com'], object, embed");
-    var $fluidEl = $("figure");
+    var $allVideos = $("iframe[src^='//www.youtube.com'], object, embed"),
+        $fluidEl = $("figure");
 
     $allVideos.each(function () {
 
-        $(this).attr('data-aspectRatio', this.height / this.width)
-               .removeAttr('height')
-               .removeAttr('width');
+        $(this)
+            // jQuery .data does not work on object/embed elements
+            .attr('data-aspectRatio', this.height / this.width)
+            .removeAttr('height')
+            .removeAttr('width');
     });
 
     $(window).resize(function () {
@@ -108,29 +110,9 @@ $(document).ready(function () {
         var newWidth = $fluidEl.width();
         $allVideos.each(function () {
             var $el = $(this);
-            $el.width(newWidth)
-               .height(newWidth * $el.attr('data-aspectRatio'));
+            $el
+                .width(newWidth)
+                .height(newWidth * $el.attr('data-aspectRatio'));
         });
     }).resize();
 });
-
-
-
-
-
-    // GET TITLE FROM YOUTUBE PLAYLIST
-    // $.get(
-    //     'https://www.googleapis.com/youtube/v3/playlists', {
-    //         part: 'snippet',
-    //         key: key,
-    //         maxResults: 10,
-    //         channelId: channelId
-    //     },
-    //     function (data) {
-    //         console.log(data);
-    //         $.each(data.items, function (i, item) {
-    //                 var playlistTitle = item.snippet.title;
-    //                 document.getElementById("playlist-title").textContent = playlistTitle;
-    //         });
-    //     }
-    // )
